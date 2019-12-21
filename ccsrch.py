a84cff749343f0dfe94e4152888fbbee3ba8b983
Jeffrey Clark <jclark@nmi.com>
2019-12-20 21:36:51 -0600

    lint

--- ccsrch.py
+++ ccsrch.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
+
 from os.path import walk, join, isfile, abspath
-import subprocess
-import sys
 from scan import Scanner
 from logger import Logger
 from optparse import OptionParser
+
 
 def visitor(arg, dirname, names):
     for name in names:
@@ -16,19 +17,19 @@ def visitor(arg, dirname, names):
 
 if __name__ == "__main__":
     parser = OptionParser(usage="usage: %prog [options] path")
-    parser.add_option("-p", "--pan-log", dest="pan_log_filename", 
+    parser.add_option("-p", "--pan-log", dest="pan_log_filename",
                       help="write PANs to this file, default is stdout",
                       default=None)
     parser.add_option("-e", "--error-log", dest="error_log_filename",
-                       help="write error log to this file, off by default",
-                       default=None)
+                      help="write error log to this file, off by default",
+                      default=None)
     parser.add_option("-i", "--ignore-log", dest="ignore_log_filename",
                       help="write ignore log to this file, off by default",
                       default=None)
     (options, args) = parser.parse_args()
 
     Logger(options.pan_log_filename, options.error_log_filename,
-            options.ignore_log_filename)
+           options.ignore_log_filename)
 
     if args and args[0]:
         path = args[0]
