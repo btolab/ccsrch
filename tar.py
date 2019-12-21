35b4d077509a0e90e2763dc4fba966f4d1a2c5b9
Jeffrey Clark <jclark@nmi.com>
2019-12-20 21:41:42 -0600

    lint

--- tar.py
+++ tar.py
@@ -3,33 +3,34 @@ from logger import Logger
 import scan
 import StringIO
 
+
 def plugin(filename, data=None):
         if data is not None:
-                try:
-                        #filelikeObj = StringIO.StringIO(data)
-                        filelikeObj = StringIO.StringIO(data)
-                        tar = tarfile.open(fileobj=filelikeObj)
-                except tarfile.TarError as e:
-                        Logger().log_error(e)
-                        return
+            try:
+                # filelike_obj = StringIO.StringIO(data)
+                filelike_obj = StringIO.StringIO(data)
+                tar = tarfile.open(fileobj=filelike_obj)
+            except tarfile.TarError as e:
+                Logger().log_error(e)
+                return
         else:
-                try:
-                        tar = tarfile.open(filename)
-                except tarfile.TarError as e:
-                        Logger().log_error(e)
-                        return
+            try:
+                tar = tarfile.open(filename)
+            except tarfile.TarError as e:
+                Logger().log_error(e)
+                return
         """ For each file in the tarball, run it through the scanner """
         for file in tar.getmembers():
-                innerFile = tar.extractfile(file)
-                if not innerFile:
-                        continue
-                try:
-                        innerFileContent = innerFile.read()
-                except tarfile.TarError as e:
-                        Logger().log_error(e)
-                        continue
-                if not innerFileContent:
-                        continue
-                scan.Scanner().scan(filename + "/" + file.name, innerFileContent)
+            inner_file = tar.extractfile(file)
+            if not inner_file:
+                continue
+            try:
+                inner_file_content = inner_file.read()
+            except tarfile.TarError as e:
+                Logger().log_error(e)
+                continue
+            if not inner_file_content:
+                continue
+            scan.Scanner().scan(filename + "/" + file.name, inner_file_content)
 
         tar.close()
